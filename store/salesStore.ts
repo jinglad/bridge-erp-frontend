fc98e8b1a7bd1af0f86c214d8c98b94e92be1cd2
shibli21 <syedshiblimahmud@gmail.com>
2022-10-09 12:15:50 +0600

    refactor code

--- store/salesStore.ts
+++ store/salesStore.ts
@@ -20,52 +20,54 @@ interface SalesStore {
 
 const useSalesStore = create<SalesStore>()(
   devtools(
-    persist((set) => ({
-      customerName: "",
-      setCustomerName: (name: string) => set({ customerName: name }),
-      productName: "",
-      setProductName: (name: string) => set({ productName: name }),
-      brandName: "",
-      setBrandName: (name: string) => set({ brandName: name }),
-      categoryName: "",
-      setCategoryName: (name: string) => set({ categoryName: name }),
-      cartItems: [],
-      addToCart: (product: Product) => {
-        set((state) => {
-          const item = state.cartItems.findIndex((item) => item._id === product._id);
+    persist(
+      (set) => ({
+        customerName: "",
+        setCustomerName: (name: string) => set({ customerName: name }),
+        productName: "",
+        setProductName: (name: string) => set({ productName: name }),
+        brandName: "",
+        setBrandName: (name: string) => set({ brandName: name }),
+        categoryName: "",
+        setCategoryName: (name: string) => set({ categoryName: name }),
+        cartItems: [],
+        addToCart: (product: Product) => {
+          set((state) => {
+            const item = state.cartItems.findIndex((item) => item._id === product._id);
 
-          if (item !== -1) {
-            const newCartItems = [...state.cartItems];
-            newCartItems[item].qty += 1;
+            if (item !== -1) {
+              const newCartItems = [...state.cartItems];
+              newCartItems[item].qty += 1;
+              return {
+                cartItems: newCartItems,
+              };
+            }
             return {
-              cartItems: newCartItems,
+              cartItems: [...state.cartItems, { ...product }],
             };
-          }
-          return {
-            cartItems: [...state.cartItems, { ...product }],
-          };
-        });
-      },
-      setCartItems: (cartItems: Product[]) => {
-        set(() => ({
-          cartItems,
-        }));
-      },
-      deleteItemFromCart: (productId: string) => {
-        set((state) => ({
-          cartItems: state.cartItems.filter((item) => item._id !== productId),
-        }));
-      },
-      reset: () => {
-        set((state) => ({
-          cartItems: [],
-          customerName: "",
-        }));
-      },
-    })),
-    {
-      name: "sales-store",
-    }
+          });
+        },
+        setCartItems: (cartItems: Product[]) => {
+          set(() => ({
+            cartItems,
+          }));
+        },
+        deleteItemFromCart: (productId: string) => {
+          set((state) => ({
+            cartItems: state.cartItems.filter((item) => item._id !== productId),
+          }));
+        },
+        reset: () => {
+          set((state) => ({
+            cartItems: [],
+            customerName: "",
+          }));
+        },
+      }),
+      {
+        name: "sales-store",
+      }
+    )
   )
 );
 
